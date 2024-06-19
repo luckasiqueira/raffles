84d3a42fa76913e3b07505d197fd2477fd678927
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-18 22:33:41 -0300

    Create info.go

---
+++ utils/info.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+var Env = info()
+
+// info reads our .env file, and get every key|value as map of strings
+func info() map[string]string {
+	data, err := godotenv.Read()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return data
+}
