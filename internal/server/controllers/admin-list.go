08096173d611555954f81e5c4f6155ce59f88404
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-19 00:51:26 -0300

    Update: now rendering templ component with all draws details

--- internal/server/controllers/admin-list.go
+++ internal/server/controllers/admin-list.go
@@ -3,9 +3,11 @@ package controllers
 import (
 	"github.com/kataras/iris/v12"
 	"raffles/internal/frontend/components"
+	"raffles/internal/server/database"
 )
 
 // AdminList handles /admin/list
 func AdminList(c iris.Context) {
-	c.RenderComponent(components.AdminList())
+	draws := database.DrawList()
+	c.RenderComponent(components.AdminList(draws))
 }
