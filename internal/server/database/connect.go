6ee7480410a387a60366510c13d7681d3380b40b
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-18 23:49:23 -0300

    Create connect.go

---
+++ internal/server/database/connect.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"raffles/utils/info"
+)
+
+var dsn = fmt.Sprintf("%s:%s@tcp(%:%s)/%s", info.Env["DB_USER"], info.Env["DB_PASS"], info.Env["DB_HOST"], info.Env["DB_PORT"], info.Env["DB_NAME"])
+
+// Connect starts a database connection using .env data
+func Connect() *sql.DB {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return db
+}
