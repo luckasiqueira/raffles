84b698f58224dee17b1cb11da17eb2846a56da2b
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-18 23:35:19 -0300

    Add: admin party and /list route

--- internal/server/routes/router.go
+++ internal/server/routes/router.go
@@ -1,10 +1,17 @@
 package routes
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"raffles/internal/server/controllers"
+)
 
 // Router defines all routes accepted for the server, and sets it's methods and respective controllers
 func Router(s *iris.Application) {
 	s.Get("/", func(c iris.Context) {
 		c.WriteString("Hello World!")
 	})
+	admin := s.Party("/admin")
+	{
+		admin.Get("/list", controllers.AdminList)
+	}
 }
