dcc41b8fe11666109e0d74411e0cef8847307988
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-18 23:43:57 -0300

    Update: moved info.go as a package info

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"github.com/kataras/iris/v12"
 	"raffles/internal/server/routes"
-	"raffles/utils"
+	"raffles/utils/info"
 )
 
 // StartServer runs the iris web server and initialize all the defined routes
 func StartServer() {
 	s := iris.New()
 	routes.Router(s)
-	err := s.Listen(":" + utils.Env["SERVER_PORT"])
+	err := s.Listen(":" + info.Env["SERVER_PORT"])
 	if err != nil {
 		fmt.Println(err.Error())
 	}
