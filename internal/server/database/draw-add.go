f8a30810cc73e6965f1d265186d76e89d2bf293c
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-20 19:12:45 -0300

    Fix: table name

--- internal/server/database/draw-add.go
+++ internal/server/database/draw-add.go
@@ -1,20 +1,25 @@
 package database
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // DrawAdd connects do DB to create new participant's draws
 func DrawAdd(number int, name, contact string) int {
 	db := Connect()
 	var count int
-	err := db.QueryRow("SELECT COUNT(*) FROM `buyers` WHERE `Draw` = ?;", number).Scan(&count)
+	err := db.QueryRow("SELECT COUNT(*) FROM `participants` WHERE `Draw` = ?;", number).Scan(&count)
 	if err != nil {
+		fmt.Println(err.Error())
 		return http.StatusInternalServerError
 	}
 	if count > 0 {
 		return http.StatusConflict
 	}
-	_, err = db.Exec("INSERT INTO `buyers` (`Name`, `Draw`, `Contact`) VALUES (?, ?, ?);", name, number, contact)
+	_, err = db.Exec("INSERT INTO `participants` (`Name`, `Draw`, `Contact`) VALUES (?, ?, ?);", name, number, contact)
 	if err != nil {
+		fmt.Println(err.Error())
 		return http.StatusInternalServerError
 	}
 	defer db.Close()
