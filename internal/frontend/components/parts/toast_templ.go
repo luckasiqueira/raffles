4a93e0def671fdd9f3758a88ec153805cc4bd9a2
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-20 00:42:40 -0300

    Update toast_templ.go

--- internal/frontend/components/parts/toast_templ.go
+++ internal/frontend/components/parts/toast_templ.go
@@ -48,7 +48,7 @@ func Toast(class, message string) templ.Component {
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(message)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal\frontend\components\parts\toast.templ`, Line: 19, Col: 46}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal\frontend\components\parts\toast.templ`, Line: 19, Col: 45}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
