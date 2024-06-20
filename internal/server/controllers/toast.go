e7aedfaca93aa399348e506ed37e92112065b88c
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-20 19:13:37 -0300

    Update: Now using http const as status and added conflict message

--- internal/server/controllers/toast.go
+++ internal/server/controllers/toast.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/kataras/iris/v12"
+	"net/http"
 	"raffles/internal/frontend/components/parts"
 )
 
@@ -10,12 +11,14 @@ func setNotification(status int, c iris.Context) {
 	var message string
 	var class = "bg-green-500"
 	switch status {
-	case 201:
+	case http.StatusCreated:
 		message = "Cadastro realizado com sucesso!"
-	case 202:
-		message = "Palpite alterado com sucesso"
-	case 204:
-		message = "Palpite removido com sucesso"
+	case http.StatusAccepted:
+		message = "Palpite alterado com sucesso!"
+	case http.StatusNoContent:
+		message = "Palpite removido com sucesso!"
+	case http.StatusConflict:
+		message = "Esse palpite já foi cadastrado!"
 	}
 	c.RenderComponent(parts.Toast(class, message))
 }
