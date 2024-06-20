43773db17eba126dd1666fa8a4b24d4f50d842bb
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-20 18:59:01 -0300

    Create admin-add.go

---
+++ internal/server/controllers/admin-add.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"github.com/kataras/iris/v12"
+	"net/http"
+	"raffles/internal/server/database"
+	"strconv"
+)
+
+func DrawAdd(c iris.Context) {
+	name := c.FormValue("name")
+	contact := c.FormValue("contact")
+	n := c.FormValue("number")
+	number, err := strconv.Atoi(n)
+	var status int
+	if err != nil {
+		status = http.StatusInternalServerError
+	}
+	status = database.DrawAdd(number, name, contact)
+	setNotification(status, c)
+}
