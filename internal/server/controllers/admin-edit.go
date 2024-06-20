8648be74c7109381caf4149e6c90bfc0380bc969
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-20 00:31:25 -0300

    Update: add form colletion and upadate logic + notification call

--- internal/server/controllers/admin-edit.go
+++ internal/server/controllers/admin-edit.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"raffles/internal/frontend/components/parts"
 	"raffles/internal/server/database"
+	"strconv"
 )
 
 // DrawEdit handles /admin/edit/{number} (GET)
@@ -19,5 +20,17 @@ func DrawEdit(c iris.Context) {
 
 // DrawEditPost handles /admin/edit/{number} (POST)
 func DrawEditPost(c iris.Context) {
-	//
+	oldNumber, err := c.Params().GetInt("number")
+	if err != nil {
+		c.StopWithStatus(http.StatusBadRequest)
+	}
+	name := c.FormValue("name")
+	n := c.FormValue("draw")
+	number, err := strconv.Atoi(n)
+	if err != nil {
+		c.StopWithStatus(http.StatusBadRequest)
+	}
+	contact := c.FormValue("contact")
+	status := database.DrawEdit(oldNumber, number, name, contact)
+	setNotification(status, c)
 }
