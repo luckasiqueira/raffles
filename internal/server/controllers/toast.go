3098e3927d9f407cfc789760deb3d8a7800b4cb9
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-20 21:25:58 -0300

    Update: added icons customization

--- internal/server/controllers/toast.go
+++ internal/server/controllers/toast.go
@@ -6,10 +6,15 @@ import (
 	"raffles/internal/frontend/components/parts"
 )
 
+const success = "fa-solid fa-check"
+const alert = "fa-solid fa-triangle-exclamation"
+const error = "fa-solid fa-xmark"
+
 // setNotification receives the status from every action, and creates a message to render properly the toast component
 func setNotification(status int, c iris.Context) {
 	var message string
 	var class = "bg-green-500"
+	var icon = success
 	switch status {
 	case http.StatusCreated:
 		message = "Cadastro realizado com sucesso!"
@@ -19,6 +24,12 @@ func setNotification(status int, c iris.Context) {
 		message = "Palpite removido com sucesso!"
 	case http.StatusConflict:
 		message = "Esse palpite já foi cadastrado!"
+		class = "bg-orange-500"
+		icon = alert
+	default:
+		message = "Houve um erro inesperado. Tente de novo!"
+		class = "bg-red-500"
+		icon = error
 	}
-	c.RenderComponent(parts.Toast(class, message))
+	c.RenderComponent(parts.Toast(class, message, icon))
 }
