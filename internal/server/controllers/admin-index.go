83f46918fbec84a63fe2ff58866fe7767cd2830f
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-06-23 21:09:27 -0300

    Update: added status rendering to templ

--- internal/server/controllers/admin-index.go
+++ internal/server/controllers/admin-index.go
@@ -3,9 +3,11 @@ package controllers
 import (
 	"github.com/kataras/iris/v12"
 	"raffles/internal/frontend/components"
+	"raffles/internal/server/database"
 )
 
 // DrawList handles /admin/
 func Home(c iris.Context) {
-	c.RenderComponent(components.Home())
+	status := database.DrawStatus()
+	c.RenderComponent(components.Home(status))
 }
