8e410b287f6184268b3b0a40a700022f2e54fb35
luckasiqueira <66996105+luckasiqueira@users.noreply.github.com>
2024-08-09 13:59:49 -0300

    Update: added winner and rud-draw routes

--- internal/server/routes/router.go
+++ internal/server/routes/router.go
@@ -15,8 +15,10 @@ func Router(s *iris.Application) {
 		admin.Get("/list/{number:int8}", controllers.DrawListSingle)
 		admin.Get("/edit/{number:int8}", controllers.DrawEdit)
 		admin.Get("/add", controllers.DrawAdd)
+		admin.Get("/winner", controllers.DrawWinner)
 		admin.Post("/add", controllers.DrawAddPost)
 		admin.Post("/edit/{number:int8}", controllers.DrawEditPost)
+		admin.Post("/run-draw", controllers.DrawRun)
 		admin.Delete("/delete/{number:int8}", controllers.DrawDelete)
 		admin.Delete("/remove-toast", func(c iris.Context) {
 			c.HTML("<div id=\"notify\"></div>")
